f30ee398ba31f0e1ba2141bf0caaf03ae5bd6475
Mennolt van Alten <mennoltvanalten01@gmail.com>
2023-02-10 17:06:14 +0100

    Check detection no longer looks under 0

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from Classes.Board import Board
 from Classes.NextMovesGenerator import NextMovesGenerator
 
 board = Board(5, q4=True)
-
-board.make_move((0, 3), (3, 4))
-board.make_move((1, 4), (2, 0))
-
-print(board)
-
-# Check possible moves
-loc_piece = [0, 4]
-piece = board.board[loc_piece[0], loc_piece[1]]
-
-# pawn at 1,0
+piece = board.board[0,4]
 next_moves_gen = NextMovesGenerator(board, piece.player)
-directions = piece.directions()
+print(next_moves_gen.detect_check(board.board, 0))
 
-for direction in directions:
-    moves = next_moves_gen.check_possible_moves_others(loc_piece, direction)
-    print(direction.direction, next_moves_gen.possible_moves)
+# board.make_move((0, 3), (3, 4))
+# board.make_move((1, 4), (2, 0))
+#
+# print(board)
+#
+# # Check possible moves
+# loc_piece = [0, 4]
+# piece = board.board[loc_piece[0], loc_piece[1]]
+#
+# # pawn at 1,0
+# next_moves_gen = NextMovesGenerator(board, piece.player)
+# directions = piece.directions()
+#
+# for direction in directions:
+#     moves = next_moves_gen.check_possible_moves_others(loc_piece, direction)
+#     print(direction.direction, next_moves_gen.possible_moves)
