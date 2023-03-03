1566e8e47a4b00ddff37a60a5fede19ac09307a6
Bram <bramreinders97@gmail.com>
2023-03-03 09:16:06 +0100

    queues in array

--- A2/Classes/Simulation.py
+++ A2/Classes/Simulation.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Simulation:
     def __init__(self, arrivalDist, pickFoodDist, serveCustCardDist, serveCustCashDist, extension_1=False, extension_2=False, extension_3=False):
         self.arrivalDist = arrivalDist
@@ -10,9 +13,7 @@ class Simulation:
 
     def simulate(self):
         T = 3600  # one hour -> 3600 s
-        q1 = 0  # length first queue
-        q2 = 0  # length second queue
-        q3 = 0  # length third queue
+        queues = np.zeros(3)  # the three queues start with 0 customers
         t = 0  # time
         n_groups_arrived = 0  # number of groups that arrived
 
