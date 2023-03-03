1e80b54ee65bb44560574e88c482fa47b99dff4c
Mennolt van Alten <mennoltvanalten01@gmail.com>
2023-03-03 15:59:37 +0100

    Added more specific generators

--- A2/Classes/Generators.py
+++ A2/Classes/Generators.py
@@ -12,3 +12,21 @@ def geo_distr(mean):
 #exponential distribution:
 def expo_distr(lam):
     return np.random.exponential(1/lam)
+
+def next_group_arriving(lam):
+    """
+    Returns time until the next group arrives at a group arriving event
+    :return: float
+    """
+    return np.random.exponential(1/lam)
+
+def time_for_getting_food(n):
+    """
+    n times for a group of n people to get food
+    :param n: int
+    :return: array[float]
+    """
+    return np.random.exponential(1 / 80, size=n)
+
+#cash: np.random.exponential(1/20)
+#card: np.random.exponential(1/12)
