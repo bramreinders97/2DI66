e2c522715a357fcd9e8c085faded32c72ec4332c
h1v3r <hiver.butterfly@gmail.com>
2023-03-04 10:43:20 +0100

    Allival event logic implemented

--- A2/Classes/Event.py
+++ A2/Classes/Event.py
@@ -1,13 +1,52 @@
+from A2.Classes.Customer import Customer
+
+import random
+
 class Event:
 
     ARRIVAL = 0
     ENTER_QUEUE = 1
     DEPARTURE = 2
 
-    def __init__(self, event_type, time, customer):
+    def __init__(self, event_type, t, customer=None):
         self.type = event_type
-        self.time = time
+        self.t = t
         self.customer = customer
 
+    def handle_arrival_event(self, group_id):
+
+        new_events = []
+
+        # TODO: Distribution
+        n_group_members = 3
+
+        # TODO: Poisson Process
+        time_till_next_arrival = 120    # The time until the next group arrives
+
+        # Create an ENTER_QUEUE event for all customers in the group
+        for i in range(n_group_members):
+
+            # Decide for cash or card
+            tmp = random.random()
+            if tmp <= 0.4:
+                payment_method = "cash"
+            else:
+                payment_method = "card"
+
+            # Decide how long the Customer needs to get the food
+            # TODO: Distribution
+            get_food_time = 80
+
+            tmp_customer = Customer(self.t, payment_method, group_id, get_food_time)
+            new_events.append(Event(1, self.t + get_food_time, tmp_customer))
+
+        # Create one new ARRIVAL event.
+        new_events.append(Event(0, self.t + time_till_next_arrival))
+
+        return new_events
+
     def __lt__(self, other):
-        return self.time < other.time
+        return self.t < other.t
+
+    def __str__(self):
+        return "Type: " + str(self.type) + "   Time: " + str(self.t)
--- A2/Classes/Simulation.py
+++ A2/Classes/Simulation.py
@@ -1,20 +1,52 @@
+from A2.Classes.Event import Event
+
 import numpy as np
+import heapq
 
 
 class Simulation:
-    def __init__(self, arrivalDist, pickFoodDist, serveCustCardDist, serveCustCashDist, extension_1=False, extension_2=False, extension_3=False):
-        self.arrivalDist = arrivalDist
-        self.pickFoodDist = pickFoodDist
-        self.serveCustCardDist = serveCustCardDist
-        self.serveCustCashDist = serveCustCashDist
-        self.extension_1 = extension_1
-        self.extension_2 = extension_2
-        self.extension_3 = extension_3
+    #def __init__(self, arrivalDist, pickFoodDist, serveCustCardDist, serveCustCashDist, extension_1=False, extension_2=False, extension_3=False):
+    #    self.arrivalDist = arrivalDist
+    #    self.pickFoodDist = pickFoodDist
+    #    self.serveCustCardDist = serveCustCardDist
+    #    self.serveCustCashDist = serveCustCashDist
+    #    self.extension_1 = extension_1
+    #    self.extension_2 = extension_2
+    #    self.extension_3 = extension_3
+
+    def __init__(self):
+        self.T = 3600   # one hour -> 3600 s
+        self.t = 0      # starting time
+
+        self.event_list = []
+
+        self.next_group_id = 0  # A counter to track the next group ID
+
 
     def simulate(self):
-        T = 3600  # one hour -> 3600 s
-        queues = np.zeros(3)  # the three queues start with 0 customers
-        t = 0  # time
-        n_groups_arrived = 0  # number of groups that arrived
 
-        # Add all logic corresponding to possible events
+        # Push first event onto the event_list
+        event = Event(0, self.t)
+        heapq.heappush(self.event_list, (event.t, event))
+
+        # Main loop
+        while self.t < self.T:
+
+            # get next element from the event list
+            event = heapq.heappop(self.event_list)[1]
+            self.t = event.t
+
+            if 0 == event.type:
+                tmp_events = event.handle_arrival_event(self.next_group_id)
+                self.next_group_id += 1
+                self.schedule_events(tmp_events)
+
+            print(self.t)
+
+        print(len(self.event_list))
+
+    def schedule_events(self, events):
+
+        for event in events:
+            heapq.heappush(self.event_list, (event.t, event))
+
