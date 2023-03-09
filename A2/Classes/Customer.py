25f6931c2476f5300460ec92a7c83334d56c2435
Bram <bramreinders97@gmail.com>
2023-03-09 20:50:28 +0100

    commit

--- A2/Classes/Customer.py
+++ A2/Classes/Customer.py
@@ -1,8 +1,8 @@
 import random
 
+
 class Customer:
     def __init__(self, arrival_time, group_index, card_only):
-
         """
         Class that represents a customer.
 
@@ -12,16 +12,20 @@ class Customer:
         """
 
         self.arrival_time = arrival_time    # Arrival-time of the customer.
-        self.departure_time = -1            # Departure-time of the customer. -1 if still in the system.
+        # Departure-time of the customer. -1 if still in the system.
+        self.departure_time = -1
 
-        self.payment_method = self.get_payment_method(card_only)    # can either be "cash" or "card".
-        self.get_food_time = self.get_food_time()   # time the customer needs to find the products he/she likes.
+        # can either be "cash" or "card".
+        self.payment_method = self.get_payment_method(card_only)
+        # time the customer needs to find the products he/she likes.
+        self.get_food_time = self.get_food_time()
 
-        self.group_index = group_index  # The index of the group the customer belongs to.
-        self.queue = None               # The queue to which the customer belongs to.
+        # The index of the group the customer belongs to.
+        self.group_index = group_index
+        # The queue to which the customer belongs to.
+        self.queue = None
 
     def get_payment_method(self,  card_only):
-
         """
         Decides the payment method of the customer. This is done at random if not specified otherwise.
 
@@ -35,13 +39,18 @@ class Customer:
             return "cash"
 
     def get_food_time(self):
-
         """
         Decides how long the customer needs to get his/her food. The decision is made according to a distribution.
         """
 
         # Decide how long the Customer needs to get the food
         # TODO: Distribution
-        return 80
+        return 5
 
+    def log_enter_queue_time(self, t):
+        """
+        Register the time a customer enters a queue
 
+        :param t: float. The current time
+        """
+        self.enter_queue_time = t
--- A2/Classes/Simulation.py
+++ A2/Classes/Simulation.py
@@ -1,27 +1,18 @@
 from A2.Classes.Event import Event
 from A2.Classes.Queues import Queues
-
-import numpy as np
+from A2.Classes.SimResults import SimulateResults
 import heapq
 
+
 class Simulation:
-    #def __init__(self, arrivalDist, pickFoodDist, serveCustCardDist, serveCustCashDist, extension_1=False, extension_2=False, extension_3=False):
-    #    self.arrivalDist = arrivalDist
-    #    self.pickFoodDist = pickFoodDist
-    #    self.serveCustCardDist = serveCustCardDist
-    #    self.serveCustCashDist = serveCustCashDist
-    #    self.extension_1 = extension_1
-    #    self.extension_2 = extension_2
-    #    self.extension_3 = extension_3
 
     def __init__(self, queue_speeds=[1, 1, 1], mobile_store=0, card_only=False):
-
         """
         Class that does one singe simulation.
 
-        :param queue_speeds:   array.   Determines how many queses there are (values of the array entries)
-                                        1 => Cashier works at 100% speed.
-                                        and how fast they are (length of the array)
+        :param queue_speeds:   array.   Determines how many queses there are (length of the array)
+                                        1 => Cashier works at 100% speed. Set one value to 1.25 for extension 1
+                                        and how fast they are (values of the array entries)
         :param mobile_store:   float.   Determines whether there is a mobile store and how many groups are using it.
                                         0.15 => 15% of the Groups go to the mobile store. Value must be between 0 and 1.
         :param card_only:      bool.    Determines whether only payment by card is accepted.
@@ -31,22 +22,26 @@ class Simulation:
         self.T = 3600           # End time: one hour -> 3600 s
         self.t = 0              # starting time
 
-        self.queue_speeds = queue_speeds  # Determines how many queses there are and how fast they are
-        self.mobile_store = mobile_store  # Determines whether there is a mobile store and how many groups are using it.
-        self.card_only = card_only        # Determines whether only payment by card is accepted.
+        # Determines how many queses there are and how fast they are
+        self.queue_speeds = queue_speeds
+        # Determines whether there is a mobile store and how many groups are using it.
+        self.mobile_store = mobile_store
+        # Determines whether only payment by card is accepted.
+        self.card_only = card_only
 
         self.event_list = []    # Event list
 
         self.next_group_id = 0  # A counter to track the next group ID
 
-        self.finished_customer_list = []  # A list of all finished customers
-        self.group_time_list = []         # A list of a 2D array: (max serving time, nr. of group members in the system)
+        # Keep track of the number of customers in the canteen at all times
+        self.n_people_in_canteen = 0
 
     def simulate(self):
-
         """
         Carries out one single simulation.
         """
+        # Create object to keep track of results
+        results = SimulateResults(ext_1=bool(sum(self.queue_speeds) > 3))
 
         # Create Queues
         queues = Queues()
@@ -58,46 +53,76 @@ class Simulation:
         heapq.heappush(self.event_list, (event.t, event))
 
         # Main loop
-        while self.t < self.T:
+        # Note that we will not receive new customers after t=3600. This is done
+        # by not scheduling new arrivals in event.handle_arrival_event() after this moment.
+        # This choice is made such that we can let every customer who is still in the canteen
+        # at t=3600 leave before calculating the results
+        while self.event_list:
 
             # get next element from the event list
             event = heapq.heappop(self.event_list)[1]
+            prev_time = self.t
             self.t = event.t
 
+            # print(f"New event at t = {self.t}. Event: {event}")
+
             if 0 == event.type:
-                tmp_events = event.handle_arrival_event(self.next_group_id, self.mobile_store, self.card_only)
+                tmp_events, n_new_people = event.handle_arrival_event(
+                    self.next_group_id, self.mobile_store, self.card_only)
                 self.next_group_id += 1
                 self.schedule_events(tmp_events)
 
+                # register a new group arrival in the results Class
+                results.registerGroupArrival()
+
+                # register the number of people that were in the canteen before this new arrival
+                results.registerNPeopleCanteen(
+                    self.t - prev_time, self.n_people_in_canteen)
+
+                # update n people in canteen
                 # -1 because there is one type 0 event which must be excluded.
-                self.group_time_list.append([-1, len(tmp_events)-1])
+                self.n_people_in_canteen += n_new_people
+
+                # print("Extra people: ", n_new_people)
 
             elif 1 == event.type:
                 tmp_events = event.handle_enter_queue_event(queues)
                 self.schedule_events(tmp_events)
 
+                # Event though the #customers does not change, update the hist because self.t will change
+                results.registerNPeopleCanteen(
+                    self.t - prev_time, self.n_people_in_canteen)
+
             elif 2 == event.type:
 
                 # update queue
-                updated_queue = event.handle_departure_event(queues.queues[event.customer.queue])
+                updated_queue = event.handle_departure_event(
+                    queues.queues[event.customer.queue])
                 queues.queues[event.customer.queue] = updated_queue
 
                 # update and save customer
                 event.customer.departure_time = self.t
-                self.finished_customer_list.append(event.customer)
 
-                # update group_time_list
-                customer_time = event.customer.departure_time - event.customer.arrival_time
-                max_time = max(self.group_time_list[event.customer.group_index][0], customer_time)
-                self.group_time_list[event.customer.group_index][0] = max_time
-                self.group_time_list[event.customer.group_index][1] -= 1
+                slow_cashier = bool(
+                    queues.queues[event.customer.queue].cashier_speed > 1)
+                print('slow: ', slow_cashier)
+
+                # Register departure in Results Class
+                results.registerDeparture(event.customer, slow_cashier)
+
+                results.registerNPeopleCanteen(
+                    self.t - prev_time, self.n_people_in_canteen)
+
+                # update n people in canteen
+                self.n_people_in_canteen -= 1
 
             else:
                 print("ERROR: in simulate: unknown event type")
                 break
 
-    def schedule_events(self, events):
+        return results
 
+    def schedule_events(self, events):
         """
         Schedules all events. The events to be scheduled are given in the argument and are pushed into the event list.
 
@@ -106,4 +131,3 @@ class Simulation:
 
         for event in events:
             heapq.heappush(self.event_list, (event.t, event))
-
