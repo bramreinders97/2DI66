218576fb2acf06b989767a1d57ae4e2c274bf7ef
Bram <bramreinders97@gmail.com>
2023-03-12 14:59:05 +0100

    wann pull

--- A2/Classes/Simulation.py
+++ A2/Classes/Simulation.py
@@ -72,21 +72,13 @@ class Simulation:
         # This choice is made such that we can let every customer who is still in the canteen
         # at t=3600 leave before calculating the results
         while self.event_list:
-            # Note that we will not receive new customers after t=3600. This is done
-            # by not scheduling new arrivals in event.handle_arrival_event() after this moment.
-            # This choice is made such that we can let every customer who is still in the canteen
-            # at t=3600 leave before calculating the results
-        while self.event_list:
-
             # get next element from the event list
             event = heapq.heappop(self.event_list)[1]
             prev_time = self.t
             prev_time = self.t
             self.t = event.t
 
-            # print(f"New event at t = {self.t}. Event: {event}")
-
-            # print(f"New event at t = {self.t}. Event: {event}")
+            print(f"New event at t = {self.t}. Event: {event}")
 
             if 0 == event.type:
                 tmp_events, n_new_people = event.handle_arrival_event(
@@ -104,18 +96,7 @@ class Simulation:
                     self.t - prev_time, self.n_people_in_canteen)
 
                 # update n people in canteen
-                # register a new group arrival in the results Class
-                results.registerGroupArrival()
-
-                # register the number of people that were in the canteen before this new arrival
-                results.registerNPeopleCanteen(
-                    self.t - prev_time, self.n_people_in_canteen)
-
-                # update n people in canteen
                 # -1 because there is one type 0 event which must be excluded.
-                self.n_people_in_canteen += n_new_people
-
-                # print("Extra people: ", n_new_people)
                 self.n_people_in_canteen += n_new_people
 
                 # print("Extra people: ", n_new_people)
@@ -128,15 +109,9 @@ class Simulation:
                 results.registerNPeopleCanteen(
                     self.t - prev_time, self.n_people_in_canteen)
 
-                # Event though the #customers does not change, update the hist because self.t will change
-                results.registerNPeopleCanteen(
-                    self.t - prev_time, self.n_people_in_canteen)
-
             elif 2 == event.type:
 
                 # update queue
-                updated_queue = event.handle_departure_event(
-                    queues.queues[event.customer.queue])
                 updated_queue = event.handle_departure_event(
                     queues.queues[event.customer.queue])
                 queues.queues[event.customer.queue] = updated_queue
@@ -154,26 +129,12 @@ class Simulation:
                 results.registerNPeopleCanteen(
                     self.t - prev_time, self.n_people_in_canteen)
 
-                slow_cashier = bool(
-                    queues.queues[event.customer.queue].cashier_speed > 1)
-                print('slow: ', slow_cashier)
-
-                # Register departure in Results Class
-                results.registerDeparture(event.customer, slow_cashier)
-
-                results.registerNPeopleCanteen(
-                    self.t - prev_time, self.n_people_in_canteen)
-
-                # update n people in canteen
-                self.n_people_in_canteen -= 1
                 # update n people in canteen
                 self.n_people_in_canteen -= 1
 
             else:
                 print("ERROR: in simulate: unknown event type")
                 break
-
-        return results
 
         return results
 
