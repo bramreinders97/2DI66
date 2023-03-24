4109863207fab0fd1ae31e2c6eabe41db7882141
h1v3r <hiver.butterfly@gmail.com>
2023-03-24 17:43:09 +0100

    code for question 3 added

--- A3/Classes/Answer.py
+++ A3/Classes/Answer.py
@@ -15,7 +15,18 @@ class Answer:
     def question_2(self):
         pass
 
-    def question_3(self, n_runs=10000, sim_time=8*60, elevators=[1, 2, 3, 4, 5]):
+    def question_3(self, n_runs=10000, sim_time=8*60, elevators=[1, 2, 3, 4, 5], n_floors=5):
+
+        """
+        Method to answer question 3.
+
+        :param n_runs:      The number of simulations
+        :param sim_time:    The time each simulation runs.
+        :param elevators:   List of values for the total number of elevators the system has.
+        :param n_floors:    The number of floors of the system. (always 5 in our case)
+        """
+
+        prob_not_to_enter = np.zeros((len(elevators), n_floors, n_runs))
 
         # Iterate over the number of runs.
         for i in range(n_runs):
@@ -23,6 +34,7 @@ class Answer:
             # Display how far the simulation is.
             if i % 100 == 0:
                 print("\r Game: " + str(i) + "/" + str(n_runs), end="")
+            print()
 
             # Iterate over different numbers of elevators
             for j in range(len(elevators)):
@@ -31,6 +43,26 @@ class Answer:
                 results = simulation.simulate()
                 results.make_calculations()
 
+                for m in range(n_floors):
+                    prob_not_to_enter[j][m][i] = results.prob_not_to_enter[m]
+
+        # Calculate the mean, standard deviation and half width.
+        mean_per_elevator_and_floor = np.zeros((len(elevators), n_floors))
+        sd_per_elevator_and_floor = np.zeros((len(elevators), n_floors))
+        hw_per_elevator_and_floor = np.zeros((len(elevators), n_floors))
+        for i in range(len(elevators)):
+            for j in range(n_floors):
+                mean_per_elevator_and_floor[i][j] = np.mean(prob_not_to_enter[i][j])
+                sd_per_elevator_and_floor[i][j] = np.std(prob_not_to_enter[i][j])
+                hw_per_elevator_and_floor[i][j] = 1.96 * np.sqrt(sd_per_elevator_and_floor[i][j] ** 2 / n_runs)
+
+        # print the results
+        print("probabilities matrix")
+        print(mean_per_elevator_and_floor)
+        print()
+        print("half widths matrix")
+        print(hw_per_elevator_and_floor)
+
     def question_4(self):
         pass
 
@@ -38,6 +70,14 @@ class Answer:
         pass
 
     def question_6(self, n_runs=10000, sim_time=8*60, elevators=[1, 2, 3, 4, 5]):
+
+        """
+        Method to answer question 6.
+
+        :param n_runs:      The number of simulations
+        :param sim_time:    The time each simulation runs.
+        :param elevators:   List of values for the total number of elevators the system has.
+        """
 
         # mean and sd of waiting times over all runs
         mean_waiting_times = np.zeros((len(elevators), n_runs))
