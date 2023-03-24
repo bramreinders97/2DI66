05be281e23499507db49b58cd21ea033bacf8716
h1v3r <hiver.butterfly@gmail.com>
2023-03-24 16:46:09 +0100

    question 6 finished

--- A3/Classes/Answer.py
+++ A3/Classes/Answer.py
@@ -24,15 +24,22 @@ class Answer:
     def question_5(self):
         pass
 
-    def question_6(self, n_runs=10, sim_time=8*60, elevators=[1, 2, 3, 4, 5]):
+    def question_6(self, n_runs=10000, sim_time=8*60, elevators=[1, 2, 3, 4, 5]):
 
-        # Total mean waiting times over all runs
+        # mean and sd of waiting times over all runs
         mean_waiting_times = np.zeros((len(elevators), n_runs))
-        # total mean waiting times over all runs including people who left the system due to impatience.
+        sd_waiting_times = np.zeros((len(elevators), n_runs))
+        # mean and sd of waiting times over all runs including people who left the system due to impatience.
         mean_waiting_times_2 = np.zeros((len(elevators), n_runs))
+        sd_waiting_times_2 = np.zeros((len(elevators), n_runs))
 
         # Iterate over the number of runs.
         for i in range(n_runs):
+
+            # Display how far the simulation is.
+            if i % 100 == 0:
+                print("\r Game: " + str(i) + "/" + str(n_runs), end="")
+
             # Iterate over different numbers of elevators
             for j in range(len(elevators)):
 
@@ -44,12 +51,33 @@ class Answer:
                 # sum up the results.
                 mean_waiting_times[j][i] = results.overall_mean_waiting_time
                 mean_waiting_times_2[j][i] = results.overall_mean_waiting_time_2
+                sd_waiting_times[j][i] = results.overall_sd_waiting_time
+                sd_waiting_times_2[j][i] = results.overall_sd_waiting_time_2
 
         # Calculate mean waiting times.
         mean_waiting_time = np.mean(mean_waiting_times, 1)
         mean_waiting_time_2 = np.mean(mean_waiting_times_2, 1)
 
         # Calculate mean standard deviations.
+        sd_waiting_time = np.mean(sd_waiting_times, 1)
+        sd_waiting_time_2 = np.mean(sd_waiting_times_2, 1)
+
+        # Calculate half widths.
+        half_width = 1.96 * np.sqrt(sd_waiting_time**2 / n_runs)
+        half_width_2 = 1.96 * np.sqrt(sd_waiting_time_2**2 / n_runs)
+
+        print("results for question 6")
+        print("elevators: " + str(elevators))
+        print("#################################")
+        print("mean waiting time: " + str(mean_waiting_time))
+        print("standard deviation: " + str(sd_waiting_time))
+        print("half width: " + str(half_width))
+        print("---------------------------------")
+        print("mean waiting time 2: " + str(mean_waiting_time_2))
+        print("standard deviation: " + str(sd_waiting_time_2))
+        print("half width: " + str(half_width_2))
+        print("#################################")
+
 
 
 
--- A3/main.py
+++ A3/main.py
@@ -2,11 +2,6 @@ from A3.Classes.Simulation import Simulation
 from A3.Classes.Answer import Answer
 
 
-#answer = Answer()
+answer = Answer()
 
-#answer.question_6()
-
-simulation = Simulation(10000, 1)
-results = simulation.simulate(True)
-results.make_calculations()
-print(results)
+answer.question_6()
