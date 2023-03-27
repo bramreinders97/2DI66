b3e7cfbf0d74cbae15632f9b79f62b924e85d5a3
h1v3r <hiver.butterfly@gmail.com>
2023-03-27 15:18:53 +0200

    some things deleted

--- A3/Classes/SimResults.py
+++ A3/Classes/SimResults.py
@@ -43,30 +43,6 @@ class SimulateResults:
         #variable for question 4
         self.chance_over_5_min_wait = None
 
-    def add_to_list_of_persons(self, person, time):
-
-        """
-        Method that adds a person to the list of finished persons.
-
-        :param person:  The person that is to be added.
-        :param time:    The time after which the person should be saved. (warm up time)
-        """
-
-        if time > self.warm_up:
-            self.list_of_persons.append(person)
-
-    def add_to_list_impatient_persons(self, person, time):
-
-        """
-        Method that adds a person to the list of persons who took the stairs because of impatience.
-
-        :param person:  The person that is to be added.
-        :param time:    The time after which the person should be saved. (warm up time)
-        """
-
-        if time > self.warm_up:
-            self.list_impatient_persons.append(person)
-
     def make_calculations(self, cut_off=2028):
 
         """
