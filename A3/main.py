1de4714df16ec106c4a8b281cd4beb5f10947c65
Bram <bramreinders97@gmail.com>
2023-03-30 11:03:29 +0200

    wanna pull

--- A3/main.py
+++ A3/main.py
@@ -2,5 +2,5 @@ from A3.Classes.Answer import Answer
 
 ans = Answer()
 
-ans.warm_up_cut_off(n_elevators=8, T=60*60*8,
+ans.warm_up_cut_off(n_elevators=2, T=60*60*8,
                     n_runs=10000, modulo_for_printing=8)
